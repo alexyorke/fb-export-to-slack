c0b32cc7327a3d29898edbe0d273b9fbd3da33a8
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2020-01-24 22:08:11 +0000

    Fix bad encoding in JSON input

--- fb-export-to-slack.py
+++ fb-export-to-slack.py
@@ -1,13 +1,21 @@
 import json
 import csv
 import sys
+import re
+from functools import partial
 
 channel = input(
     "Enter name of Slack channel to import to (without # prefix): ")
 
+# https://stackoverflow.com/a/50011987
+fix_mojibake_escapes = partial(
+     re.compile(rb'\\u00([\da-f]{2})').sub,
+     lambda m: bytes.fromhex(m.group(1).decode()))
+
 fbMessages = []
-with open(sys.argv[1]) as json_file:
-    data = json.load(json_file)
+with open(sys.argv[1], 'rb') as binary_data:
+    json_file = fix_mojibake_escapes(binary_data.read())
+    data = json.loads(json_file.decode('utf8'))
     for message in data['messages']:
         if (message['type'] == "Generic") and ('sticker' not in message):
 		    # messages are in milliseconds; have to divide by 1000 to get epoch
